fb31766e7b06840f0a9b45e46f9d80443d9b4f2d
danielrobinson92 <danielrobinson92@users.noreply.github.com>
2023-09-17 18:28:51 +0530

    dynamic component

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,19 @@
 // Javascript program
 // for now it does not receive any arguments
 const App = ()=>{
+  const now = new Date();
+  const a = 10;
+  const b = 20;
+
+  console.log(a+b);
   //  it can also execute or contain js code
-  console.log("Hello from component app")
-  
+  // console.log("Hello from component app")
+
   // Returns HTML
   return (
   <div>
-    <p>Hello world</p>
+    <p>Hello world, it is {now.toString()}</p>
+    <p> {a} + {b} = {a+b}</p>
   </div>
 )
 }
