48d460407cfbc8d7befbf8ea26f3f800bec5087b
danielrobinson92 <danielrobinson92@users.noreply.github.com>
2023-09-26 07:45:17 +0530

    Skeleton format completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,92 @@
 import './App.css';
 
-const Hello = (props)=>{
-  console.log(props);
+const price = (plan,price)=>{
   return(
-    <div>
-      <p>Hello {props.name}, your age is {props.age}</p>
-    </div>
+    <div className='header-div'><p className='plan'>{plan}</p>
+      <h2>${price}/Month</h2>
+    </div> 
   )
 }
 
-const App = ()=>{
+const free_plan = ()=>{
+  return(
+  <div>
+    <ul style={{listStyleType:"none"}}>
+      <li>Single User</li>
+      <li>50GB Storage</li>
+      <li>Unlimited Public Projects</li>
+      <li>Community Access</li>
+    </ul>
+    <ul style={{listStyleType:"none"}}>
+      <li>Unlimited Private Projects</li>
+      <li>Dedicated Phone Support</li>
+      <li>Free Subdomain</li>
+      <li>Monthly Status Reports</li>
+    </ul>
+  </div>
+)}
 
-  let inlineStyle = {
-    color:'white',
-    background:'red'
-  }
+const plus_plan = ()=>{
+  return(
+  <div>
+    <ul style={{listStyleType:"none"}}>
+      <li>Single User</li>
+      <li>50GB Storage</li>
+      <li>Unlimited Public Projects</li>
+      <li>Community Access</li>
+      <li>Unlimited Private Projects</li>
+      <li>Dedicated Phone Support</li>
+      <li>Free Subdomain</li>
+    </ul>
+    <ul style={{listStyleType:"none"}}>
+      <li>Monthly Status Reports</li>
+    </ul>
+  </div>
+)}
+
+const pro_plan = ()=>{
+  return(
+  <div>
+    <ul style={{listStyleType:"none"}}>
+      <li>Single User</li>
+      <li>50GB Storage</li>
+      <li>Unlimited Public Projects</li>
+      <li>Community Access</li>
+      <li>Unlimited Private Projects</li>
+      <li>Dedicated Phone Support</li>
+      <li>Free Subdomain</li>
+      <li>Monthly Status Reports</li>
+    </ul>
+  </div>
+)}
+
+
+
+
+
+const App = ()=>{
 
   // Returns HTML
   return (
-  <div>
-    <h1 style={{color:"white", background:"green"}}> Greetings</h1>
-    <Hello style={inlineStyle}name ="Daniel" age ="30" />
-    <Hello name ="Darwin" age ="28" />
+  <div id = 'flex' className='flex'>
+      <div className='box'>
+        {price('FREE',0)}
+        {free_plan()}
+      </div>
+    <div className='box'>
+        {price('PLUS',9)}
+        {plus_plan()}
+    </div>
+    <div className='box'>
+        {price('PRO',49)}
+        {pro_plan()}
+    </div> 
   </div>
 )
 }
 export default App;
+
+  // let inlineStyle = {
+  //   color:'white',
+  //   background:'red'
+  // }
