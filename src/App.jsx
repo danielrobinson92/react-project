5cbbf461c453570ba7f8a96bbfa323ca968059ec
danielrobinson92 <danielrobinson92@users.noreply.github.com>
2023-09-27 09:06:27 +0530

    Working with array lists - hardcorded the list items

--- src/App.jsx
+++ src/App.jsx
@@ -1,103 +1,23 @@
-import './App.css';
 
-const price = (plan,price)=>{
-  return(
-    <div className='header-div'><p className='plan'>{plan}</p>
-      <h2>${price}/Month</h2>
-    </div> 
+
+function App(props) {
+  console.log(props);
+
+  let {notes} = props;
+  console.log(notes);
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        <li>{notes[0].content}</li>
+        <li>{notes[1].content}</li>
+        <li>{notes[2].content}</li>
+        <li>{notes[3].content}</li>
+        <li>{notes[4].content}</li>
+      </ul>
+    </div>
   )
 }
 
-const free_plan = ()=>{
-  return(
-  <div className='plan_div'>
-    <ul style={{listStyleType:"none"}}>
-      <li>Single User</li>
-      <li>50GB Storage</li>
-      <li>Unlimited Public Projects</li>
-      <li>Community Access</li>
-    </ul>
-    <ul style={{listStyleType:"none"}}>
-      <li>Unlimited Private Projects</li>
-      <li>Dedicated Phone Support</li>
-      <li>Free Subdomain</li>
-      <li>Monthly Status Reports</li>
-    </ul>
-  </div>
-)}
-
-const plus_plan = ()=>{
-  return(
-  <div className='plan_div'>
-    <ul style={{listStyleType:"none"}}>
-      <li>Single User</li>
-      <li>50GB Storage</li>
-      <li>Unlimited Public Projects</li>
-      <li>Community Access</li>
-      <li>Unlimited Private Projects</li>
-      <li>Dedicated Phone Support</li>
-      <li>Free Subdomain</li>
-    </ul>
-    <ul style={{listStyleType:"none"}} >
-      <li>Monthly Status Reports</li>
-    </ul>
-  </div>
-)}
-
-const pro_plan = ()=>{
-  return(
-  <div className='plan_div'>
-    <ul style={{listStyleType:"none"}}>
-      <li>Single User</li>
-      <li>50GB Storage</li>
-      <li>Unlimited Public Projects</li>
-      <li>Community Access</li>
-      <li>Unlimited Private Projects</li>
-      <li>Dedicated Phone Support</li>
-      <li>Free Subdomain</li>
-      <li>Monthly Status Reports</li>
-    </ul>
-  </div>
-)}
-
-const button=()=>{
-  return (
-    <div>
-      <button>Button</button>
-    </div>
-  )}
-
-
-
-const App = ()=>{
-
-  // Returns HTML
-  return (
-  <div id = 'flex' className='flex'>
-      <div className='box'>
-        {price('FREE',0)}
-        <hr/>
-        {free_plan()}
-        {button()}
-      </div>
-    <div className='box'>
-        {price('PLUS',9)}
-        <hr/>
-        {plus_plan()}
-        {button()}
-    </div>
-    <div className='box'>
-        {price('PRO',49)}
-        <hr/>
-        {pro_plan()}
-        {button()}
-    </div> 
-  </div>
-)
-}
-export default App;
-
-  // let inlineStyle = {
-  //   color:'white',
-  //   background:'red'
-  // }
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -2,4 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+
+// Data
+const notes = [
+    { id: 1, content: 'Working with Lists', important: true},
+    { id: 2, content: 'Usage of Keys', important: false},
+    { id: 3, content: 'COndition Rendering', important: true},
+    { id: 4, content: 'Handling Events', important: true},
+    { id: 5, content: 'Props vs State', important: false}
+]
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App notes={notes}/>);
