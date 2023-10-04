03add487209671c62dfc3b8a74c8aa9368e5d89f
danielrobinson92 <danielrobinson92@users.noreply.github.com>
2023-10-04 08:44:20 +0530

    Passing props in functional components

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,21 @@
-/*
-Hooks: 
-  -any functions that startts with "use" is called a hook.
-  -they are special functions that are only available while Reach is rendering
+import React from 'react'
 
-To create a counter, where the value is increased as a function of time or at the click of the button
-
-*/
-
-import React from "react";
-
-function GrandChildComponent({parentData}){
-  console.log(`from grand child componenet :${parentData}`);
-
+function Hello(props){
   return(
-    <div>
-      <h3>Grand Child Component</h3>
-    </div>
+    <div>Hello, {props.name}!</div>
+
   )
-}
 
-
-
-function ChildComponent({parentData}){
-  console.log(`from Child Component ${parentData}`);
-  return (
-    <div>
-        <h1>Child Component</h1>
-        <GrandChildComponent parentData={parentData} />
-    </div>
-  )
 }
 
 
 
 function App() {
-
-  const parentData = 'Hello from Parent';
-  return(
+  return (
     <div>
-        <h1>Parent Component</h1>
-        <ChildComponent parentData={parentData} />;
+      <Hello name = "Krish" />
     </div>
   )
 }
 
-export default App;
+export default App
